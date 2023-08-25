44bb4a811d94a05b9e0c63fbe882b96da2e75c36
lolcode <1123603312@qq.com>
2023-08-25 12:44:37 +0800

    重新整理最新片段及添加复现gif动图

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,39 @@
 <template>
-  <div style="width: 256px">
-    <Button type="primary" @click="toggleCollapsed">{{ state.collapsed ? '展开' : '收起' }}</Button>
-
-    <Menu
-      v-model:openKeys="state.openKeys"
-      :selectedKeys="state.selectedKeys"
-      :inline-collapsed="state.collapsed"
-      :items="menus"
-      mode="inline"
-      theme="dark"
-    ></Menu>
-  </div>
+  <button @click="inlineCollapsed = !inlineCollapsed">tagg</button>
+  <Menu v-model:selectedKeys="current" mode="inline" :items="items" :inlineCollapsed="inlineCollapsed" style="width: 200px" />
 </template>
 
-<script setup>
-import { reactive, computed, createVNode } from 'vue';
-import { Button, Menu } from 'ant-design-vue'
-import { HomeFilled, AccountBookFilled } from '@ant-design/icons-vue'
-
-const state = reactive({
-  collapsed: false,
-  // collapsed: true,
-  selectedKeys: ['test1'],
-  openKeys: ['key1'],
-  preOpenKeys: []
-})
-
-const menus = computed(() => [
+<script lang="ts" setup>
+import { h, ref, computed } from 'vue';
+import { Menu } from 'ant-design-vue'
+import { AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+const current = ref<string[]>(['mail']);
+const inlineCollapsed = ref(false)
+const items = computed(() => [
   {
-    key: 'key1',
-    title: 'test1',
-    icon: () => createVNode(HomeFilled),
-    label: 'test1',
+    key: 'sub1',
+    icon: () => h(SettingOutlined),
+    label: 'Navigation Three - Submenu',
+    title: 'Navigation Three - Submenu',
     children: [
       {
-        key: 'key1-1',
-        title: 'test1-1',
-        label: createVNode(HomeFilled) // 用图标组件代替 实际情况是RouterLink组件
+        type: 'group',
+        label: 'Item 1',
+        children: [
+          {
+            label: h(AppstoreOutlined),
+            title: 'setting:1',
+            key: 'setting:1',
+          },
+          {
+            label: 'Option 2',
+            key: 'setting:2',
+          },
+        ],
       }
-    ]
-  },
-  {
-    key: 'key2',
-    title: 'test2',
-    icon: () => createVNode(AccountBookFilled),
-    label: 'test2',
-    children: [
-      {
-        key: 'key2-1',
-        title: 'test2-1',
-        label: createVNode(AccountBookFilled)
-      },
-      {
-        key: 'key2-2',
-        title: 'test2-2',
-        label: createVNode(AccountBookFilled)
-      }
-    ]
+    ],
   }
-])
+]);
 
-// menus.value  // 触发下menus.value就可以
-
-const toggleCollapsed = () => {
-  state.collapsed = !state.collapsed;
-  if (state.collapsed) {
-    state.openKeys = []
-  }
-}
+// items.value 执行下item.value就能暂时性解决此问题
 </script>
